847479633330f44ce06bb8be927f362dbca89223
elit-code <eliastan596@gmail.com>
2025-03-28 10:46:51 +0100

    paar features

--- src/Dozierender.java
+++ src/Dozierender.java
@@ -3,11 +3,21 @@ public class Dozierender {
     private String fakultaet;
     private int bueroNummer;
 
+    public Dozierender(String name, String fakultaet, int bueroNummer) {
+        this.name = name;
+        this.fakultaet = fakultaet;
+        this.bueroNummer = bueroNummer;
+    }
+
     public void frageBeantworten(String frage) {
         String antwort;
         System.out.println("Frage: " + frage);
         System.out.println("Bitte geben Sie Ihre Antwort ein:");
         antwort = Input.readString();
         System.out.println("Antwort: " + antwort);
+    }
+
+    public void FeedbackErhalten(String feedback) {
+        System.out.println("Feedback erhalten: " + feedback);
     }
 }
