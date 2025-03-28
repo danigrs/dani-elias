ffd81e465dc6c487c605030d1fbefb4ae1b609dc
elit-code <eliastan596@gmail.com>
2025-03-28 10:46:51 +0100

    paar features

--- src/App.java
+++ src/App.java
@@ -1,7 +1,12 @@
 public class App {
     public static void main(String[] args) throws Exception {
-        System.out.println("Hello, World!");
-        System.out.println("servus");
-        System.out.println("servus");
+        Dozierender dozierender = new Dozierender("Simon Nestler", "Informatik", 101);
+        Studierender studierender = new Studierender("Max Mustermann", "Informatik", 123456);
+        
+        String frage = studierender.frageStellen();
+        dozierender.frageBeantworten(frage);
+        System.out.println("Bitte geben Sie Ihr Feedback ein: ");
+        String feedback = Input.readString();
+        dozierender.FeedbackErhalten(feedback);
     }
 }
--- src/Studierender.java
+++ src/Studierender.java
@@ -2,7 +2,13 @@ public class Studierender {
     String name;
     int matrikelnummer;
     String studiengang;
+    
+    public Studierender(String name, String studiengang, int matrikelnummer) {
+        this.name = name;
+        this.studiengang = studiengang;
+        this.matrikelnummer = matrikelnummer;
 
+    }
     public String frageStellen() {
         System.out.println("Bitte geben Sie Ihre Frage ein: ");
         String question = Input.readString();
