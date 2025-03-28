25549437d333d1a8f655192004b0546186dfc084
elit-code <eliastan596@gmail.com>
2025-03-28 12:58:15 +0100

    asdf

--- src/App.java
+++ src/App.java
@@ -1,15 +1,5 @@
 public class App {
     public static void main(String[] args) throws Exception {
-<<<<<<< HEAD
-        Dozierender dozierender = new Dozierender("Simon Nestler", "Informatik", 101);
-        Studierender studierender = new Studierender("Max Mustermann", "Informatik", 123456);
-        
-        String frage = studierender.frageStellen();
-        dozierender.frageBeantworten(frage);
-        System.out.println("Bitte geben Sie Ihr Feedback ein: ");
-        String feedback = Input.readString();
-        dozierender.FeedbackErhalten(feedback);
-=======
         // Dozierender und Studierender erstellen
         Dozierender dozierender = new Dozierender("Simon Nestler", "Informatik", 101);
         Studierender studierender = new Studierender("Max Mustermann", "Informatik", 123456);
@@ -42,6 +32,5 @@ public class App {
         praktikum.setDetails("Objektorientierte Programmierung", 10, "Raum 101", "01.04.2025");
         praktikum.aufgabenEinlesen();
         praktikum.getDetails();
->>>>>>> a49f6328100c26d15a0ae9c3de1a4bc79eea84a2
     }
 }
